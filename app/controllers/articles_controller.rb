30c887dbe8b88562bf80cded0669bb8d2933b9c9
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-14 17:16:08 +0100

    created edit and update methods and redered partial form in the view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :edit, :update]
 
   def index
     @articles = Article.all
@@ -16,6 +16,15 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
 
     redirect_to article_path(@article)
   end
