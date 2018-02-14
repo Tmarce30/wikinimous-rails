888b8cbacc14a15dabb2b58ef8ec58dccb3fcfb5
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-14 17:06:40 +0100

    created created method and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,18 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
   private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 
   def set_article
     @article = Article.find(params[:id])
